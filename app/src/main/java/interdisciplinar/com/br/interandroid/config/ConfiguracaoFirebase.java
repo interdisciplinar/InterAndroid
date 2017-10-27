6dcab4df07241860a4108cb4cb80be93f4189ca7
JeanPierre <jeanpsantos@unifebe.edu.br>
2017-10-26 22:25:30 -0200

    Configurando o Firebase

---
+++ app/src/main/java/interdisciplinar/com/br/interandroid/config/ConfiguracaoFirebase.java
@@ -0,0 +1,10 @@
+package interdisciplinar.com.br.interandroid.config;
+
+import com.google.firebase.database.DatabaseReference;
+
+public final class ConfiguracaoFirebase {
+
+//    private DatabaseReference databaseReference =
+
+
+}

379f64cb5737d18e76aab1afd267a75009c2fdc9
José Renato de Souza <joserenato@unifebe.edu.br>
2017-11-05 13:18:53 -0200

    Renato

---
+++ app/src/main/java/interdisciplinar/com/br/interandroid/HomeCliente.java
@@ -0,0 +1,33 @@
+package interdisciplinar.com.br.interandroid;
+
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ImageButton;
+
+public class HomeCliente extends AppCompatActivity {
+    private ImageButton btnBuscar;
+    private ImageButton btnAgendar;
+    private ImageButton btnRecentes;
+    private ImageButton btnMapa;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_home_cliente);
+
+        btnMapa = (ImageButton) findViewById(R.id.Mapa);
+
+        btnMapa.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+                startActivity(new Intent(HomeCliente.this, MapsActivity.class));
+                finish();
+            }
+        });
+
+    }
+}
---
+++ app/src/main/java/interdisciplinar/com/br/interandroid/HomeEmpresa.java
@@ -0,0 +1,13 @@
+package interdisciplinar.com.br.interandroid;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class HomeEmpresa extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_home_empresa);
+    }
+}
